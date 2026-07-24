0401ec15c17e2a77e5cac6a55a899222242e0c17
CarlosAlbeiro <carlosalbeirogaleano@hotmail.com>
2026-07-24 11:57:23 -0500

    Cambio de puerto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,7 @@ import { whatsappBot } from './services/whatsappBot';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3055;
 
 app.use(cors());
 app.use(express.json());
